aefac63495e0c3a9400b55c859fe6d7da296333a
erika122333 <santi200325@hotmail.es>
2025-11-16 21:22:11 -0500

    Initial commit: subir proyecto completo

--- server/index.ts
+++ server/index.ts
@@ -70,12 +70,14 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+ const port = parseInt(process.env.PORT || '5000', 10);
+
+server.listen(
+  port,
+  process.platform === "win32" ? "127.0.0.1" : "0.0.0.0",
+  () => {
+    log(`Servidor corriendo en http://${process.platform === "win32" ? "localhost" : "0.0.0.0"}:${port}`);
+  }
+);
+
 })();
